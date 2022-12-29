ef28ae909cf8ee4b52eb55d9ef97eb84e6df8fb0
Alejandro Roa <alejandro.roa@workiasolutions.com>
2022-12-29 14:38:36 -0400

    some configurations

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ItemsModule } from './items/items.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb+srv://alejo:12345@cluster0.w9odzkm.mongodb.net/?retryWrites=true&w=majority'), ItemsModule],
+  imports: [MongooseModule.forRoot('mongodb+srv://alejo:12345@cluster0.w9odzkm.mongodb.net/?retryWrites=true&w=majority'), ItemsModule, AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
