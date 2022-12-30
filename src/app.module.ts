3866bea1a55e3de84d155cdf7931c3c6bd0c281a
Alejandro Roa <alejandro.roa@workiasolutions.com>
2022-12-30 15:09:01 -0400

    config

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,20 @@ import { AppService } from './app.service';
 import { ItemsModule } from './items/items.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config'
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb+srv://alejo:12345@cluster0.w9odzkm.mongodb.net/?retryWrites=true&w=majority'), ItemsModule, AuthModule, UsersModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env'],
+    }),
+    MongooseModule.forRoot(
+      process.env.MONGO_URI
+    ),
+    ItemsModule, 
+    AuthModule, 
+    UsersModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
