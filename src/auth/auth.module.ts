963c0d54c47ac8ed680429ca6de99cb8c0049534
Alejandro Roa <alejandro.roa@workiasolutions.com>
2022-12-31 13:20:38 -0400

    test

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,7 +4,7 @@ import { AuthController } from './auth.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from 'src/users/schema/user.schema';
 import { JwtModule } from '@nestjs/jwt';
-import { jwtConstanst } from 'src/jwt.constanst';
+import { jwtConstanst } from '../../src/jwt.constanst';
 import { JwtStrategy } from './jwt.strategy';
 @Module({
   imports: [
